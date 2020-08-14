0a8493bf81f4dc3d42dc6572dec3f7632cd7d602
Kaya Jackson <kayajackson27@gmail.com>
2020-08-14 13:05:37 -0400

    second commit

--- js/script.js
+++ js/script.js
@@ -3,18 +3,17 @@
 //**form stuff**
 const focusFirstField = document.querySelector('input[type=text]');
 const jobList = document.getElementById('title');
-const otherJob = document.getElementById('other');
 const input = document.getElementById('other-title');
 
 
 //focus on first text field using the focus method
 focusFirstField.focus();
 
-//if else statement for the text box to appear when 'other' is selected
-const showOtherInput = (e) => {
+
+jobList.addEventListener('change', (e) => {
     if (e.target.value === 'other') {
-        input.style.display = '';
+        input.style.display = 'block';
     } else {
         input.style.display = 'none';
     }
-};
+});
